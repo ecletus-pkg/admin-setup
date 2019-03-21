14649f46f176138aec10faa768c78558da39e2bc
Moises P. Sena <moisespsena@gmail.com>
2019-03-21 17:37:05 -0300

    Many Changes

--- plugin.go
+++ plugin.go
@@ -3,15 +3,17 @@ package admin_setup
 import (
 	"strings"
 
+	"github.com/moisespsena-go/iolr"
+
 	"github.com/aghape-pkg/site-setup"
 	"github.com/aghape-pkg/user"
 	"github.com/aghape/auth"
-	"github.com/aghape/media"
+	"github.com/aghape/media/oss"
 	"github.com/aghape/notification"
 	"github.com/aghape/plug"
+	"github.com/moisespsena-go/aorm"
 	"github.com/moisespsena/go-default-logger"
 	"github.com/moisespsena/go-error-wrap"
-	"github.com/moisespsena/go-ioutil"
 	"github.com/moisespsena/go-path-helpers"
 )
 
@@ -19,40 +21,41 @@ var log = defaultlogger.NewLogger(path_helpers.GetCalledDir())
 
 type Plugin struct {
 	plug.EventDispatcher
-	AuthKey string
+	AuthKey, NotificationKey string
 }
 
 func (p *Plugin) RequireOptions() []string {
-	return []string{p.AuthKey}
+	return []string{p.AuthKey, p.NotificationKey}
 }
 
 func (p *Plugin) OnRegister() {
 	site_setup.OnRegister(p, func(e *site_setup.SiteSetupEvent) {
-		e.SetupCMD.Flags().String("admin-email", "", "E-mail for admin user")
+		e.SetupCMD.Flags().StringP("admin-email", "E", "", "E-mail for admin user")
+		e.SetupCMD.Flags().StringP("admin-password", "P", "", "The Password. Use BLANK for generated password.")
 	})
 
-	site_setup.OnSetup(p, func(e *site_setup.SiteSetupEvent) error {
+	site_setup.OnSetup(p, func(e *site_setup.SiteSetupEvent) (err error) {
 		site := e.Site
 		var adminUser user.User
-		db := media.IgnoreCallback(site.GetSystemDB().DB)
-		db.First(&adminUser, "name = ?", "admin")
-		if adminUser.ID == "" {
+		db := oss.IgnoreCallback(site.GetSystemDB().DB)
+		err = db.First(&adminUser, "name = ?", "admin").Error
+		if err != nil && aorm.IsRecordNotFoundError(err) {
+			err = nil
 			log.Info("Create System Administrator user")
 			var (
 				Auth         = e.Options().GetInterface(p.AuthKey).(*auth.Auth)
-				Notification = notification.New(&notification.Config{})
+				Notification = e.Options().GetInterface(p.NotificationKey).(*notification.Notification)
 			)
 			return user.CreateAdminUserIfNotExists(site, Auth, Notification, func() (string, error) {
 				adminEmail, err := e.SetupCMD.Flags().GetString("admin-email")
 				if err != nil {
-					return "", errwrap.Wrap(err, "Get admin-mail flag")
+					return "", errwrap.Wrap(err, "Get admin-email flag")
 				}
 				for adminEmail == "" {
-					line, err := ioutil.STDStringMessageLR.Read("Enter the mail address for admin user")
+					adminEmail, err = iolr.STDMessageLR.ReadS("Enter the email address for admin user")
 					if err != nil {
 						return "", errwrap.Wrap(err, "Get admin-email from STDIN")
 					}
-					adminEmail = string(line)
 					if !strings.Contains(adminEmail, "@") {
 						log.Errorf("The %q isn't valid mail address. Try now.", adminEmail)
 						adminEmail = ""
@@ -61,6 +64,12 @@ func (p *Plugin) OnRegister() {
 					break
 				}
 				return adminEmail, nil
+			}, func() (string, error) {
+				value, err := e.SetupCMD.Flags().GetString("admin-password")
+				if err != nil {
+					return "", errwrap.Wrap(err, "Get admin-password flag")
+				}
+				return value, nil
 			})
 		}
 		return nil
