aa926bcb493751f1d8c75fabc6124dbd770df80f
Moises P. Sena <moisespsena@gmail.com>
2019-04-01 17:02:59 -0300

    Update Dependencies

--- plugin.go
+++ plugin.go
@@ -12,9 +12,9 @@ import (
 	"github.com/ecletus/notification"
 	"github.com/ecletus/plug"
 	"github.com/moisespsena-go/aorm"
-	"github.com/moisespsena/go-default-logger"
+	"github.com/moisespsena-go/default-logger"
 	"github.com/moisespsena/go-error-wrap"
-	"github.com/moisespsena/go-path-helpers"
+	"github.com/moisespsena-go/path-helpers"
 )
 
 var log = defaultlogger.NewLogger(path_helpers.GetCalledDir())
